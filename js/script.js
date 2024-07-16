441fa4dccbd231817ecf75cb19cb61f6170dcb18
africanCLAM <rafir0532@gmail.com>
2024-07-16 22:12:46 +0700

    adding search bar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-//toggle class active
+/*toggle class activehamburger*/
 const navbarNav = document.querySelector('.navbar-nav');
-
 //Hamburger menu clicked
 document.querySelector('#hamburger-menu').onclick = () => { navbarNav.classList.toggle('active') };
 
-//click outside sidebar for closing
-const hamburger = document.querySelector('#hamburger-menu');
+/*toggle class active search form*/
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active'); 
+    searchBox.focus();
+    e.preventDefault();
+};
+
+//click outside for closing
+const searchButton = document.querySelector('#search-button'); //search button
+const hamburger = document.querySelector('#hamburger-menu'); //hamburger menu
 
 document.addEventListener('click', function (e) {
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 })
